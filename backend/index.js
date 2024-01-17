c95e77b2cac43a3426954667cf27653643609721
Sydney Lehman <lehman.s@northeastern.edu>
2024-01-17 15:05:35 -0500

    Integrate Github Api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,50 @@
 import express from "express";
+import cors from "cors"
+import { Octokit } from "octokit";
 
-const port = 3000
 
+// maybe you can do some sort of stats comparison app? or leaderboard or something?
+// i'm thinking you can query for people's profiles and pull stats around num of lines added/deleted, num of commits, avg commit frequency, etc
+// and then compare 2 (or multiple) profiles and see how they match up
+// and you could have different leaderboards for different stats
+// with all these stats, you could also look into some visualizing tools/packages to spice things up rather than just displaying text
+
+
+const port = 8080
 const app = express()
 
+//{ auth: `personal-access-token123` }
+const octokit = new Octokit();
+
+app.use(cors())
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true }))
+
+app.post("/compare", async (req, res) => {
+    const {user1, user2} = req.body
+    console.log(user1, user2)
+    const gitResponse1 = await octokit.request('GET /users/{username}', {
+        username: user1,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    const gitResponse2 = await octokit.request('GET /users/{username}', {
+        username: user2,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+    })
+    console.log(gitResponse1)
+    console.log(gitResponse2)
+
+    const resData = {
+        user1Data: gitResponse1.data.public_repos,
+        user2Data: gitResponse2.data.public_repos
+    }
+    res.json(resData)
+})
 
 app.listen(port, () => {
-    console.log("listening on port 3000")
+    console.log("listening on port 8080")
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,44 @@
+import React from "react"
+import axios from "axios"
 
+function App() {
+
+  const [user1, setUser1] = React.useState("")
+  const [user2, setUser2] = React.useState("")
+  // const [repo1, setRepo1] = React.useState("")
+  const [displayFlag, setDisplayFlag] = React.useState(false)
+
+  let user1Repos
+  let user2Repos
+
+  async function handleForm(e){
+    e.preventDefault()
+    const body = {user1: user1, user2: user2}
+    const response = await axios.post("http://localhost:8080/compare", body)
+    // const {user1Repos, user2Repos} = response.data
+    const responseData = response.data
+    console.log(responseData)
+   
+    setDisplayFlag(true)
+    user1Repos = responseData.user1Data
+    user2Repos = responseData.user2Data
+  }
+
+
+
+  return (
+    <div>
+      <h1>Githubbed</h1>
+      <p>Pls two valid users to get their repo counts</p>
+      <form onSubmit={handleForm}>
+      <input type="text" placeholder="User 1" value={user1} onChange={(e) => setUser1(e.target.value)} />
+      <input type="text" placeholder="User 2" value={user2} onChange={(e) => setUser2(e.target.value)}/>
+      <input type="submit" value={"Submit"}/>
+      </form>
+      {displayFlag && <div><p>{user1} public repos: {user1Repos}</p> <p>{user2} public repos: {user2Repos}</p></div>}
+      
+    </div>
+  );
+}
+
+export default App;
